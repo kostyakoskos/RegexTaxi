7c16769e98e87ab8410c697cdf3df36bab989fbf
kostyakoskos <50551774+kostyakoskos@users.noreply.github.com>
2020-03-04 13:24:00 +0300

    Main C#

---
+++ main.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Text.RegularExpressions;
+namespace Taxi
+{
+    //class main
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int count = 0;
+    //        //Console.Write("Введите количество строк");
+    //        //int n = int.Parse(Console.ReadLine());
+    //        //try
+    //        //{
+    //        //    if(n>100000 || n<1)
+    //        //    {
+    //        //        throw new ArgumentException();
+    //        //    }
+    //        //}
+    //        //catch(ArgumentException)
+    //        //{
+    //        //    Console.WriteLine("Invalid parametr.");
+    //        //}
+    //        //// ArrayList arrayList = new ArrayList();
+    //        //string[] mas = new string[n];// будет не инт, а то что нам надо!!!!
+    //        //for (int i = 0; i < mas.Length; i++)
+    //        //{
+
+    //        //    String e_mail = "name@gmail.";
+
+    //        //}
+    //        string number = "7TAX 2222";
+    //        //если номер это регулярное выражение, то count++
+    //        if (number  )
+    //    }
+
+    //}
+    //class Isvalid
+    //{
+    //    public bool isValidMail(string e_mail)
+    //    {
+    //        bool x = false;
+    //        string expr = "[7][TAXBE]/s[0-9]{4} || [1-6][TAXB]/s[0-9]{4}";
+
+    //        Match isMatch = Regex.Match(e_mail, expr, RegexOptions.IgnoreCase);
+
+    //        return isMatch.Success;
+    //    }
+    //}
+
+   
+}
